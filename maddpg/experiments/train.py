388d3cddd407f4a677afbc0fbf28bcd9fcc90d60
Chansol Hong <23516823+chshong@users.noreply.github.com>
2020-06-30 17:55:53 +0900

    reorder training cycle

--- maddpg/experiments/train.py
+++ maddpg/experiments/train.py
@@ -10,13 +10,14 @@ import rewards
 import maddpg.common.tf_util as U
 from maddpg.trainer.rmaddpg import _RMADDPGAgentTrainer
 import utils
-import os
 from network import mlp_model, lstm_fc_model
 
 os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
 def parse_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
+    # Output Filename
+    parser.add_argument("--commit_num", type=str, default="0", help="commit number?")
     # Environment
     parser.add_argument("--scenario", type=str, default="simple", help="name of the scenario script")
     parser.add_argument("--max-episode-len", type=int, default=100, help="maximum episode length")
@@ -42,9 +43,9 @@ def parse_args():
     parser.add_argument("--tracking", action="store_true", default=False)
     # Checkpointing
     parser.add_argument("--exp-name", type=str, default=None, help="name of the experiment")
-    parser.add_argument("--save-dir", type=str, default="/tmp/policy/", help="directory in which training state and model should be saved")
+    parser.add_argument("--save-dir", type=str, default="./saved_policy/", help="directory in which training state and model should be saved")
     parser.add_argument("--save-rate", type=int, default=1000, help="save model once every time this many episodes are completed")
-    parser.add_argument("--load-dir", type=str, default="/tmp/policy/", help="directory in which training state and model are loaded")
+    parser.add_argument("--load-dir", type=str, default="./saved_policy/", help="directory in which training state and model are loaded")
     parser.add_argument("--test-actor-q", action="store_true", default=False)
     # Evaluation
     parser.add_argument("--graph", action="store_true", default=False)
@@ -149,7 +150,6 @@ class trainTest():
         self.agent_info = [[[]]]  # placeholder for benchmarking info
         self.saver = tf.train.Saver()
         self.obs_n = self.env.reset()
-        self.episode_step = 0
         self.train_step = 0
         self.t_start = time.time()
         self.new_episode = True # start of a new episode (used for replay buffer)
@@ -159,20 +159,151 @@ class trainTest():
             print("Setting up graph writer!")
             self.writer = tf.summary.FileWriter("learning_curves/graph",sess.graph)
 
-    def update(self, arglist, obs_n):
         if arglist.analysis:
             print("Starting analysis on {}...".format(arglist.analysis))
             if arglist.analysis != 'video':
                 analyze.run_analysis(arglist, self.env, self.trainers)
             return # should be a single run
+
+    def update(self, arglist, obs_n, rew_n, done_n, info_n, terminal):
+        # info_n is false only when the very first data was created
+        if info_n != False:
+            done = all(done_n)
+
+            # collect experience
+            for i, agent in enumerate(self.trainers):
+                # do this every iteration
+                if arglist.critic_lstm and arglist.actor_lstm:
+                    agent.experience(self.prev_obs_n[i], self.action_n[i], rew_n[i],
+                                    obs_n[i], done_n[i], # terminal,
+                                    self.p_in_c_n[i][0], self.p_in_h_n[i][0],
+                                    self.p_out_c_n[i][0], self.p_out_h_n[i][0],
+                                    self.q_in_c_n[i][0], self.q_in_h_n[i][0],
+                                    self.q_out_c_n[i][0], self.q_out_h_n[i][0], self.new_episode)
+                elif arglist.critic_lstm:
+                    agent.experience(self.prev_obs_n[i], self.action_n[i], rew_n[i],
+                                    obs_n[i], done_n[i], # terminal,
+                                    self.q_in_c_n[i][0], self.q_in_h_n[i][0],
+                                    self.q_out_c_n[i][0], self.q_out_h_n[i][0],self.new_episode)
+                elif arglist.actor_lstm:
+                    agent.experience(self.prev_obs_n[i], self.action_n[i], rew_n[i],
+                                    obs_n[i], done_n[i], # terminal,
+                                    self.p_in_c_n[i][0], self.p_in_h_n[i][0],
+                                    self.p_out_c_n[i][0], self.p_out_h_n[i][0],
+                                    self.new_episode)
+                else:
+                    agent.experience(self.prev_obs_n[i], self.action_n[i], rew_n[i],
+                                    obs_n[i], done_n[i], # terminal,
+                                    self.new_episode)
+
+            # Adding rewards
+            if arglist.tracking:
+                for i, a in enumerate(self.trainers):
+                    # if arglist.num_episodes - len(self.episode_rewards) <= 1000:
+                    #     a.tracker.record_information("goal", np.array(self.env.world.landmarks[0].state.p_pos))
+                    #     a.tracker.record_information("position",np.array(self.env.world.agents[i].state.p_pos))
+                    a.tracker.record_information("ag_reward", rew_n[i])
+                    a.tracker.record_information("team_dist_reward", info_n["team_dist"][i])
+                    a.tracker.record_information("team_diff_reward", info_n["team_diff"][i])
+
+            # Closing graph writer
+            if arglist.graph:
+                self.writer.close()
+            for i, rew in enumerate(rew_n):
+                self.episode_rewards[-1] += rew
+                self.agent_rewards[i][-1] += rew
+
+            # If an episode was finished, reset internal values
+            if done or terminal:
+                self.new_episode = True
+                # reset trainers
+                if arglist.actor_lstm or arglist.critic_lstm:
+                    for agent in self.trainers:
+                        agent.reset_lstm()
+                if arglist.tracking:
+                    for agent in self.trainers:
+                        agent.tracker.reset()
+                self.episode_rewards.append(0)
+                for a in self.agent_rewards:
+                    a.append(0)
+                self.agent_info.append([[]])
+            else:
+                self.new_episode=False
+
+            # increment global step counter
+            self.train_step += 1
+
+            # for benchmarking learned policies
+            if arglist.benchmark:
+                for i, info in enumerate(info_n):
+                    self.agent_info[-1][i].append(info_n['n'])
+                if self.train_step > arglist.benchmark_iters and (done or terminal):
+                    file_name = arglist.benchmark_dir + arglist.exp_name + '.pkl'
+                    print('Finished benchmarking, now saving...')
+                    with open(file_name, 'wb') as fp:
+                        pickle.dump(self.agent_info[:-1], fp)
+                    return
+            # otherwise training
+            else:
+                # update all trainers, if not in display or benchmark mode
+                loss = None
+
+                # get same episode sampling
+                if arglist.sync_sampling:
+                    inds = [random.randint(0, len(self.trainers[0].replay_buffer._storage)-1) for i in range(arglist.batch_size)]
+                else:
+                    inds = None
+
+                for agent in self.trainers:
+                    # if arglist.lstm:
+                    #     agent.preupdate(inds=inds)
+                    # else:
+                    agent.preupdate(inds)
+                for agent in self.trainers:
+                    loss = agent.update(self.trainers, self.train_step)
+                    if loss is None: continue
+
+                # save model, display training output
+                if terminal and (len(self.episode_rewards) % arglist.save_rate == 0):
+                    U.save_state(arglist.save_dir, saver=self.saver)
+                    # print statement depends on whether or not there are adversaries
+                    if self.num_adversaries == 0:
+                        print("steps: {}, episodes: {}, mean episode reward: {}, time: {}".format(
+                            self.train_step, len(self.episode_rewards), np.mean(self.episode_rewards[-arglist.save_rate:]), round(time.time()-self.t_start, 3)))
+                    else:
+                        print("steps: {}, episodes: {}, mean episode reward: {}, agent episode reward: {}, time: {}".format(
+                            self.train_step, len(self.episode_rewards), np.mean(self.episode_rewards[-arglist.save_rate:]),
+                            [np.mean(rew[-arglist.save_rate:]) for rew in self.agent_rewards], round(time.time()-self.t_start, 3)))
+                    self.t_start = time.time()
+                    # Keep track of final episode reward
+                    self.final_ep_rewards.append(np.mean(self.episode_rewards[-arglist.save_rate:]))
+                    for rew in self.agent_rewards:
+                        self.final_ep_ag_rewards.append(np.mean(rew[-arglist.save_rate:]))
+
+
+        if arglist.actor_lstm:
+            # get critic input states
+            self.p_in_c_n, self.p_in_h_n = get_lstm_states('p', self.trainers) # num_trainers x 1 x 1 x 64
+        if arglist.critic_lstm:
+            self.q_in_c_n, self.q_in_h_n = get_lstm_states('q', self.trainers) # num_trainers x 1 x 1 x 64
+
+        # get action
+        self.action_n = [agent.action(obs) for agent, obs in zip(self.trainers, obs_n)]
+        if arglist.critic_lstm:
+            # get critic output states
+            p_states = [self.p_in_c_n, self.p_in_h_n] if arglist.actor_lstm else []
+            update_critic_lstm(self.trainers, obs_n, self.action_n, p_states)
+            self.q_out_c_n, self.q_out_h_n = get_lstm_states('q', self.trainers) # num_trainers x 1 x 1 x 64
+        if arglist.actor_lstm:
+            self.p_out_c_n, self.p_out_h_n = get_lstm_states('p', self.trainers) # num_trainers x 1 x 1 x 64
+
+        self.prev_obs_n = obs_n
+
+        return self.action_n
+
+
 
     def loop(self, arglist):
-        if arglist.analysis:
-            print("Starting analysis on {}...".format(arglist.analysis))
-            if arglist.analysis != 'video':
-                analyze.run_analysis(arglist, self.env, self.trainers)
-            return # should be a single run
-
         print('Starting iterations...')
         while True:
             if arglist.actor_lstm:
@@ -317,11 +448,6 @@ class trainTest():
                 for rew in self.agent_rewards:
                     self.final_ep_ag_rewards.append(np.mean(rew[-arglist.save_rate:]))
 
-            # end of iteration
-            if len(self.episode_rewards) > arglist.num_episodes:
-                return
-
-
     def finish(self, arglist):
         # saves final episode reward for plotting training curve later
         # U.save_state(arglist.save_dir, saver=saver)
@@ -329,6 +455,8 @@ class trainTest():
             for agent in self.trainers:
                 agent.tracker.save()
 
+        if not os.path.exists("rewards"):
+            os.makedirs("rewards")
         rew_file_name = "rewards/" + arglist.commit_num + "_rewards.pkl"
         with open(rew_file_name, 'wb') as fp:
             pickle.dump(self.final_ep_rewards, fp)
@@ -340,11 +468,43 @@ class trainTest():
 
         self.sess.close()
 
+    def episodes_seen(self):
+        return len(self.episode_rewards)
 
 def train(arglist):
     tt = trainTest()
     tt.init(arglist)
-    tt.loop(arglist)
+
+    #create environment
+    env = make_env(arglist.scenario, arglist, arglist.benchmark)
+    episode_step = 0
+
+    #get the first observation (rew, done, info are dummies)
+    obs_n = env.reset()
+    rew_n = False
+    done_n = False
+    info_n = False
+    terminal = False
+    print('Starting iterations...')
+    while tt.episodes_seen() <= arglist.num_episodes:
+        action = tt.update(arglist, obs_n, rew_n, done_n, info_n, terminal)
+
+        # environment step
+        obs_n, rew_n, done_n, info_n = env.step(action)
+        episode_step += 1
+
+        done = all(done_n)
+        terminal =  episode_step >= arglist.max_episode_len
+
+        # reset environment if finished
+        if done or terminal:
+            obs_n = env.reset()
+            episode_step = 0
+
+        # for displaying learned policies
+        if arglist.display:
+            env.render()
+
     tt.finish(arglist)
 
 
